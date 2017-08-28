45e8f5da035f0ba9cefba6643c77d14efb5a3a2f
daneryl <daneryl@gmail.com>
2017-08-28 18:19:16 +0200

    new cejil api url configured

--- app/react/config.js
+++ app/react/config.js
@@ -1,2 +1,3 @@
-const APIURL = process.env.API_URL;
-export default {APIURL: APIURL ? APIURL : 'https://cejil.uwazi.io/api/'};
+//const APIURL = process.env.API_URL;
+//export default {APIURL: APIURL ? APIURL : 'https://cejil.uwazi.io/api/'};
+export default {APIURL: 'https://sidh.cejil.org/api'};

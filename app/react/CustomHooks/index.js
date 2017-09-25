270c7415fc61f5204ec8b1c1162fb2364bf7da3d
RafaPolit <rafaelpolit@gmail.com>
2017-09-25 12:16:12 -0500

    Removed Leaflet

--- app/react/CustomHooks/index.js
+++ app/react/CustomHooks/index.js
@@ -1,11 +1,9 @@
 import cejilChart001 from './components/cejilChart001';
 import cejilHero from './components/cejilHero';
 import reactMapGl from './components/reactMapGl';
-import reactLeaflet from './components/reactLeaflet';
 
 export default {
   cejilChart001,
   cejilHero,
-  reactMapGl,
-  reactLeaflet
+  reactMapGl
 };

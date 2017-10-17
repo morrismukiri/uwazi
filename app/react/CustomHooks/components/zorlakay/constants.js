91beb2b81b4be45370f6a13682f8914a3b0488f7
Clément Habinshuti <hello@habbes.xyz>
2017-10-17 16:11:44 +0300

    Updated constants

--- app/react/CustomHooks/components/zorlakay/constants.js
+++ app/react/CustomHooks/components/zorlakay/constants.js
@@ -1,18 +1,18 @@
 // victim template
-export const VICTIMS = '59e0a4fff476ea0de6d4590a';
+export const VICTIMS = '59e5f0f538372b22fa77357f';
 
 /* Fields */
 export const DECISION_BY_THE_PROSECUTION_OFFICE = 'decision_by_the_prosecution_office';
 
 /* Thesauris */
 // MT25
-export const STATUS_AS_VICTIM = '59e0a4eff476ea0de6d44118';
+export const STATUS_AS_VICTIM = '59e5f0df38372b22fa771d8d';
 // MT63
-export const LOCAL_GEOGRAPHICAL_AREA = '59e0a4f8f476ea0de6d449d8';
+export const LOCAL_GEOGRAPHICAL_AREA = '59e5f0eb38372b22fa77264d';
 // MT64
-export const LOCAL_TERM_FOR_OCCUPATION = '59e0a4f9f476ea0de6d4515b';
+export const LOCAL_TERM_FOR_OCCUPATION = '59e5f0ec38372b22fa772dd0';
 
 /* Thesauri values */
 // values for Decision of prosecution office/ Autopsy results (MT34)
 // vocab 05000000000034
-export const CASE_ONGOING = 'f21a573f-fb46-4b44-84fc-4828276b73f1';
+export const CASE_ONGOING = '6a23632b-ca9b-4935-a56f-45c7ed4f987e';

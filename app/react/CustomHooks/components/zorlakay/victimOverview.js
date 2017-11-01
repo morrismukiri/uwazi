0f36e7db6dc106ec186271bdae05df098902d659
Clément Habinshuti <hello@habbes.xyz>
2017-11-01 17:25:04 +0300

    Fetch related victims and display them in victim overview

--- app/react/CustomHooks/components/zorlakay/victimOverview.js
+++ app/react/CustomHooks/components/zorlakay/victimOverview.js
@@ -1,62 +1,105 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {getThesauriItemLabel, 
-  formatDate, 
+  formatDate,
   formatCitiesAsString,
   formatOccupationsAsString,
-  extractImageDataUrlFromMarkdown } from './helpers';
+  extractImageDataUrlFromMarkdown} from './helpers';
+import {fetchRelatedVictims} from './zorlakayAPI';
 
-function VictimOverview ({victim, templates, thesauris, settings}) {
-  const idConfig = settings.collection.get('custom').get('zorlakayIds');
-  const data = victim.metadata;
-  const name = victim.title;
-  const age = data.age_at_the_time_of_victimization || 'Unknown';
-  const status = getThesauriItemLabel(thesauris, idConfig.get('thesauriStatusAsVictim'), 
-    data.status_of_the_victim_at_the_end_of_act);
-  const occupation = formatOccupationsAsString(thesauris, data.local_term_for_occupation, idConfig);
-  const location = formatCitiesAsString(thesauris, data.place_of_the_event, idConfig);
-  const date = data.initial_date? formatDate(data.initial_date) : 'Unknown';
-  const image = data.picture? extractImageDataUrlFromMarkdown(data.picture) :
-    '/public/team-placeholder.jpg';
-  
-  return (
-    <div className="video">
-      <div className="victim-details">
-        <div className="img">
-          <img src={ image } />
-        </div>
-        <div>
-          <h1>{ name }</h1>
-          <p>Age { age }, { occupation }</p>
-        </div>
-        <div>
-          <p>
-            <i className="fa fa-circle"></i> <span>Status:</span> { status }
-          </p>
-          <p>
-            <span>Lost location:</span> <a href="#"> <i className="fa fa-map-marker"></i> { location }</a>
-          </p>
-          <p>
-            <span>Lost date:</span> { date }
-          </p>
-          <p>
-            <span>Other people who lost together:</span><br />
-            <a href="#"><i className="fa fa-user-o"></i> Abdurrahman İbin</a>, <a href="#"><i className="fa fa-user-o"></i> Celal Yanık</a>
-          </p>
-        </div>
-        <div>
-          <a href="#">
-            <i className="fa fa-file-text-o"></i> View report
-          </a>
-          <a href="#">
-            <i className="fa fa-file-video-o"></i> Testimonial
-          </a>
+class VictimOverview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      relatedVictims: []
+    };
+
+    const {victim, idConfig} = this.props;
+    fetchRelatedVictims(victim.sharedId, idConfig.get('relationRelatedEvent'))
+    .then(relatedVictims => {
+      this.setState({relatedVictims});
+    });
+  }
+
+  renderRelatedVictims(victims) {
+    if (!victims.length) {
+      return <p/>;
+    }
+
+    return (
+      <p>
+        <span>Other people who lost together:</span><br />
+        {victims.map((v, i) => (
+          <span key={v.title}>
+            <a href="#"><i className="fa fa-user-o"></i> {v.title}</a>
+            {i < victims.length - 1 ? ', ' : ''}
+          </span>
+        ))}
+      </p>
+    );
+  }
+
+  render() {
+    const {victim, thesauris, idConfig} = this.props;
+    const data = victim.metadata;
+    const name = victim.title;
+    const age = data.age_at_the_time_of_victimization || 'Unknown';
+    const status = getThesauriItemLabel(thesauris, idConfig.get('thesauriStatusAsVictim'), 
+      data.status_of_the_victim_at_the_end_of_act);
+    const occupation = formatOccupationsAsString(thesauris, data.local_term_for_occupation, idConfig);
+    const location = formatCitiesAsString(thesauris, data.place_of_the_event, idConfig);
+    const date = data.initial_date ? formatDate(data.initial_date) : 'Unknown';
+    const image = data.picture ? extractImageDataUrlFromMarkdown(data.picture) :
+      '/public/team-placeholder.jpg';
+    const {relatedVictims} = this.state;
+
+    return (
+      <div className="video">
+        <div className="victim-details">
+          <div className="img">
+            <img src={ image } />
+          </div>
+          <div>
+            <h1>{ name }</h1>
+            <p>Age { age }, { occupation }</p>
+          </div>
+          <div>
+            <p>
+              <i className="fa fa-circle"></i> <span>Status:</span> { status }
+            </p>
+            <p>
+              <span>Lost location:</span> <a href="#"> <i className="fa fa-map-marker"></i> { location }</a>
+            </p>
+            <p>
+              <span>Lost date:</span> { date }
+            </p>
+            { this.renderRelatedVictims(relatedVictims)}
+          </div>
+          <div>
+            <a href="#">
+              <i className="fa fa-file-text-o"></i> View report
+            </a>
+            <a href="#">
+              <i className="fa fa-file-video-o"></i> Testimonial
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-const mapStateToProps  = ({templates, thesauris, settings}) => ({templates, thesauris, settings});
+VictimOverview.propTypes = {
+  thesauris: PropTypes.object,
+  idConfig: PropTypes.object,
+  victim: PropTypes.object
+};
+
+const mapStateToProps  = ({templates, thesauris, settings}) => ({
+  templates,
+  thesauris,
+  idConfig: settings.collection.get('custom').get('zorlakayIds')
+});
 
 export default connect(mapStateToProps)(VictimOverview);
--- app/react/CustomHooks/components/zorlakay/zorlakayAPI.js
+++ app/react/CustomHooks/components/zorlakay/zorlakayAPI.js
@@ -1,12 +1,57 @@
 import searchApi from 'app/Search/SearchAPI';
+import api from 'app/utils/api';
 
 /**
  * makes request to search for entities from the specified template
  * @param {string} template template id
  * @param {object} args additional args to pass to search api
- * @return {Promise}
+ * @return {Promise} resolves search results
  */
-export function fetchTemplateEntities (template, args = {limit: 0}) {
-    args.types = [template];
-    return searchApi.search(args);
+export function fetchTemplateEntities(template, args = {limit: 0}) {
+  args.types = [template];
+  return searchApi.search(args);
+}
+
+/**
+ * makes request to fetch references of the specified entity
+ * @param {string} entity entity sharedId
+ * @param {string} relationType relation type id, if provided only references
+ * from this relation type are returned
+ * @return {Promise} resolves with references results
+ */
+export function fetchEntityReferences(entity, relationType = null) {
+  const url = `references/by_document/${entity}`;
+  return api.get(url).then(response => {
+    if (relationType) {
+      return response.json.filter(ref => ref.relationType === relationType);
+    }
+    return response.json;
+  });
+}
+
+/**
+ * fetches all victims who are related to the same event
+ * as the specified victim
+ * @param {string} victim victim id
+ * @param {string} eventRelation relation type id
+ * @return {Promise} resolves with array of related victims
+ */
+export function fetchRelatedVictims(victim, eventRelation) {
+  return fetchEntityReferences(victim, eventRelation)
+  .then(victimRefs => {
+    if (!victimRefs.length) {
+      return Promise.resolve([]);
+    }
+    const event = victimRefs[0].connectedDocument;
+    return fetchEntityReferences(event, eventRelation)
+    .then(eventRefs => {
+      return eventRefs.filter(r => r.connectedDocument !== victim)
+      .map(r => ({
+        template: r.connectedDocumentTemplate,
+        sharedId: r.connectedDocument,
+        title: r.connectedDocumentTitle,
+        metadata: r.connectedDocumentMetadata
+      }));
+    });
+  });
 }

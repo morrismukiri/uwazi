56de3af791432e6361250d4d20119b6410d6726b
daneryl <daneryl@gmail.com>
2017-11-06 11:26:06 +0100

    victimStats as a separate component

--- app/react/CustomHooks/index.js
+++ app/react/CustomHooks/index.js
@@ -5,6 +5,7 @@ import reactMapGl from './components/reactMapGl';
 import zorlakayGraphics from './components/zorlakayGraphics';
 import ZorlakayHeader from './components/ZorlakayHeader';
 import ZorlakayVictimsMap from './components/zorlakay/VictimsMap';
+import ZorlakayVictimsStats from './components/zorlakay/HomeStats';
 
 export default {
   cejilChart001,
@@ -12,5 +13,6 @@ export default {
   reactMapGl,
   ZorlakayHeader,
   zorlakayGraphics,
-  ZorlakayVictimsMap
+  ZorlakayVictimsMap,
+  ZorlakayVictimsStats
 };

82c6abf471ce2034c1ae073a934766d2b358bd07
A Happy Cat <acasadotorres@gmail.com>
2018-01-25 17:31:39 +0100

    renamed api files and variables using references/connections to relationships

--- app/api/api.js
+++ app/api/api.js
@@ -13,7 +13,7 @@ export default (app, server) => {
   //module routes
   require('./auth/routes.js')(app);
   require('./socketio/middleware.js')(server, app);
-  require('./references/routes.js')(app);
+  require('./relationships/routes.js')(app);
   require('./users/routes.js')(app);
   require('./templates/routes.js')(app);
   require('./search/routes.js')(app);

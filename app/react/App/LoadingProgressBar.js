f3c695a7c04d29380d81eb7ddccf2203078af114
RafaPolit <rafaelpolit@gmail.com>
2018-02-02 11:22:16 -0500

    Temp test fix commenting out entire LoadingProgressBar.

--- app/react/App/LoadingProgressBar.js
+++ app/react/App/LoadingProgressBar.js
@@ -9,23 +9,23 @@ const loadingProgressBar = {
   requests: 0,
 
   start: () => {
-    if (isClient && window.NProgress) {
-      window.NProgress.configure({showSpinner: false, easing: 'ease', speed: 800, parent: '.app-content', minimum: 0.2});
-      window.NProgress.start();
-      loadingProgressBar.requests += 1;
-    }
+    // if (isClient && window.NProgress) {
+    //   window.NProgress.configure({showSpinner: false, easing: 'ease', speed: 800, parent: '.app-content', minimum: 0.2});
+    //   window.NProgress.start();
+    //   loadingProgressBar.requests += 1;
+    // }
   },
 
   done: () => {
-    if (isClient && window.NProgress) {
-      loadingProgressBar.requests -= 1;
-      if (loadingProgressBar.requests <= 0) {
-        window.NProgress.done();
-        return;
-      }
+    // if (isClient && window.NProgress) {
+    //   loadingProgressBar.requests -= 1;
+    //   if (loadingProgressBar.requests <= 0) {
+    //     window.NProgress.done();
+    //     return;
+    //   }
 
-      window.NProgress.inc(0.1);
-    }
+    //   window.NProgress.inc(0.1);
+    // }
   }
 };
 

ffe4f765569892f1e4b0cf31dea4313756889c82
Clément Habinshuti <hello@habbes.xyz>
2018-02-27 13:17:24 +0300

    Fix more linter issues in zorlakay

--- app/react/Markdown/CustomHooks/components/zorlakay/filterHub.js
+++ app/react/Markdown/CustomHooks/components/zorlakay/filterHub.js
@@ -1,101 +1,106 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import MapFilter from './mapFilter';
 
 export default class FilterHub extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            data: {
-                status: null,
-                city: null,
-                year: null
-            },
-            currentFilters: {}
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        status: null,
+        city: null,
+        year: null
+      },
+      currentFilters: {}
+    };
+  }
+
+  onFilter(filter, values) {
+    const {field, getValue} = filter;
+    const currentFilters = Object.assign({}, this.state.currentFilters);
+    currentFilters[field] = {
+      values,
+      getValue
+    };
+    const source = this.props.data;
+    const results = this.localSearch(source, currentFilters);
+
+    this.setState({currentFilters});
+    this.props.onFilter(results);
+  }
+
+  filterData(data, field, values, getValue) {
+    if (!values.length) {
+      return data;
     }
+    return data.filter(item => {
+      let fieldValues = getValue(item.metadata[field]);
+      fieldValues = Array.isArray(fieldValues) ? fieldValues : [fieldValues];
+      const res = fieldValues.find(val => values.includes(val));
+      return res;
+    });
+  }
 
-    onFilter (filter, values) {
-        const { field, getValue } = filter;
-        const currentFilters = Object.assign({}, this.state.currentFilters);
-        const thisFilter = currentFilters[field] || {};
-        const { data } = thisFilter;
-        currentFilters[field] = {
-            values,
-            getValue
-        };
-        const source = this.props.data;
-        const results = this.localSearch(source, currentFilters);
-
-        this.setState({ currentFilters });
-        this.props.onFilter(results);
+  computeAggregations(data, field, getValue) {
+    const otherFilters = Object.assign({}, this.state.currentFilters);
+    delete otherFilters[field];
+    const filteredData = this.localSearch(data, otherFilters);
+    const counts = {};
+    for (const item of filteredData) {
+      let fieldValues = getValue(item.metadata[field]);
+      fieldValues = Array.isArray(fieldValues) ? fieldValues : [fieldValues];
+      for (let value of fieldValues) {
+        counts[value] = (counts[value] || 0) + 1;
+      }
     }
+    const options = Object.keys(counts).map((key) => {
+      return {
+        key: key,
+        value: key,
+        label: key,
+        results: counts[key]
+      };
+    });
+    return options;
+  }
 
-    filterData (data, field, values, getValue) {
-        if (!values.length) return data;
-        return data.filter(item => {
-            let fieldValues = getValue(item.metadata[field]);
-            fieldValues = Array.isArray(fieldValues)? fieldValues: [fieldValues];
-            const res = fieldValues.find(val => values.includes(val));
-            return res !== undefined;
-        });
-    }
+  localSearch(data, filters) {
+    const filterFields = Object.keys(filters);
+    const filtered = filterFields.reduce((thisData, field) => {
+      const {getValue, values} = filters[field];
+      return this.filterData(thisData, field, values, getValue);
+    }, data);
+    return filtered;
+  }
 
-    computeAggregations (data, field, getValue) {
-        const otherFilters = Object.assign({}, this.state.currentFilters);
-        delete otherFilters[field];
-        const filteredData = this.localSearch(data, otherFilters);
-        const counts = {};
-        for(const item of filteredData) {
-            let fieldValues = getValue(item.metadata[field]);
-            fieldValues = Array.isArray(fieldValues)? fieldValues : [fieldValues];
-            for (let value of fieldValues) {
-                counts[value] = (counts[value] || 0) + 1;
-            }
-        }
-        const options = Object.keys(counts).map((key) => {
-            return {
-                key: key,
-                value: key,
-                label: key,
-                results: counts[key]
-            }
-        });
-        return options;
-    }
+  renderFilter(filter, data) {
+    const {getValue, field} = filter;
+    const values = field in this.state.currentFilters ?
+      this.state.currentFilters[field].values : [];
+    const options = this.computeAggregations(data, field, getValue);
+    return (
+      <MapFilter
+        key={ field }
+        { ...filter }
+        onFilter={ this.onFilter.bind(this, filter) }
+        values={ values }
+        options={ options } />
+    );
+  }
 
-    localSearch (data, filters) {
-        const filterFields = Object.keys(filters);
-        const filtered = filterFields.reduce((thisData, field) => {
-            const { getValue, values } = filters[field];
-            return this.filterData(thisData, field, values, getValue)
-        }, data);
-        return filtered;
-    }
-
-    
-
-    renderFilter (filter, data) {
-        const { title, getValue, field } = filter;
-        const values = field in this.state.currentFilters?
-            this.state.currentFilters[field].values : [];
-        const options = this.computeAggregations(data, field, getValue);
-        return (
-            <MapFilter
-                key={ field }
-                { ...filter }
-                onFilter={ this.onFilter.bind(this, filter) }
-                values={ values }
-                options={ options } />
-        );
-    }
-
-    render () {
-        const { filters, data } = this.props;
-        const filterItems = filters.map(f => this.renderFilter(f, data));
-        return (
-            <ul className='filters'>
-                { filterItems }
-            </ul>
-        );
-    }
+  render() {
+    const {filters, data} = this.props;
+    const filterItems = filters.map(f => this.renderFilter(f, data));
+    return (
+      <ul className='filters'>
+        { filterItems }
+      </ul>
+    );
+  }
 }
+
+FilterHub.propTypes = {
+  filters: PropTypes.array,
+  data: PropTypes.array,
+  onFilter: PropTypes.func
+};
--- app/react/Markdown/CustomHooks/components/zorlakay/victimMapFilters.js
+++ app/react/Markdown/CustomHooks/components/zorlakay/victimMapFilters.js
@@ -1,33 +1,43 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import FilterHub from './filterHub';
-import { getThesauriItemLabel } from './helpers';
+import {getThesauriItemLabel} from './helpers';
 
 const VictimMapFilters = ({victims, onFilter, thesauris, settings}) => {
-    const idConfig = settings.collection.get('custom').get('zorlakayIds');
-    const LOCAL_GEOGRAPHICAL_AREA = idConfig.get('thesauriLocalGeographicalArea');
-    const filters = [
-        {
-            title: 'City',
-            field: 'city',
-            getValue: (rawVal, field, obj) => {
-                if (!rawVal || !rawVal.length) return 'Unknown';
-                return rawVal.map(val => getThesauriItemLabel(thesauris, LOCAL_GEOGRAPHICAL_AREA, val))
-            }
-        },
-        {
-            title: 'Year',
-            field: 'initial_date',
-            getValue: (rawVal, field, obj) => {
-                return String(new Date(rawVal * 1000).getFullYear());
-            }
+  const idConfig = settings.collection.get('custom').get('zorlakayIds');
+  const LOCAL_GEOGRAPHICAL_AREA = idConfig.get('thesauriLocalGeographicalArea');
+  const filters = [
+    {
+      title: 'City',
+      field: 'city',
+      getValue: (rawVal) => {
+        if (!rawVal || !rawVal.length) {
+          return 'Unknown';
         }
-    ];
-    return (
-        <FilterHub data={ victims }
-            onFilter={ onFilter }
-            filters={ filters } />
-    )
+        return rawVal.map(val => getThesauriItemLabel(thesauris, LOCAL_GEOGRAPHICAL_AREA, val));
+      }
+    },
+    {
+      title: 'Year',
+      field: 'initial_date',
+      getValue: (rawVal) => {
+        return String(new Date(rawVal * 1000).getFullYear());
+      }
+    }
+  ];
+  return (
+    <FilterHub data={ victims }
+      onFilter={ onFilter }
+      filters={ filters } />
+  );
+};
+
+VictimMapFilters.propTypes = {
+  victims: PropTypes.array,
+  onFilter: PropTypes.func,
+  thesauris: PropTypes.object,
+  settings: PropTypes.object
 };
 
 const mapStateToProps = ({thesauris, settings}) => ({thesauris, settings});
